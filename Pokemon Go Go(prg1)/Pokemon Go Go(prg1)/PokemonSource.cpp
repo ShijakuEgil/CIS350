f6411708e53648d66244271799ab1ac90c84a62b
egish <egish@DESKTOP-S4HOJJJ>
2017-01-14 17:51:10 -0500

    pokrmon comit 2

--- Pokemon Go Go(prg1)/Pokemon Go Go(prg1)/PokemonSource.cpp
+++ Pokemon Go Go(prg1)/Pokemon Go Go(prg1)/PokemonSource.cpp
@@ -6,8 +6,15 @@ Prof. Bruce Elenbogen
 */
 
 /*
-The The program will take input from the user consisting of integers only. 
-The first input will be an integer n (0< n < 11) that will determine */
+The purpose of this program is to permute the different locations of pokemons given by the user 
+and give back the smallest distance that can be traveled in blocks, also the path to follow in 
+order to achieve the smallest distance. The user input will consist of only integers for the 
+locations and strings for the pokemon names. The first input will be an integer n (0 < n < 11) 
+that will determine the amount of locations that the user wants the program to permute through. 
+Each of the next n inputs will consist of 2 integers separated by a space and the name of the 
+pokemon located at that location. The output will consist of the locations in the permuted order
+that add up to the smallest distance and the distance traveled in blocks
+*/
 
 #include<iostream>
 #include<algorithm>
@@ -16,17 +23,25 @@ using namespace std;
 
 int main()
 {
-	int nrOfPokemon = 0;
+	int nrOfPokemon;
 	int north, east;
-	vector<vector<int> > blocks;
-	while (nrOfPokemon <= 0 || nrOfPokemon >= 11) 
+	string name;
+	vector<int> blocksNorth;
+	vector<int> blocksEast;
+	vector<string> pokemon;
+
+	cin >> nrOfPokemon;
+	while (nrOfPokemon <= 0 || nrOfPokemon >= 11)
 	{
 		cin >> nrOfPokemon;
 	}
 	for (int i = 0; i < nrOfPokemon; i++)
 	{
 		cin >> north >> east;
-
+		getline(cin,name);
+		blocksNorth.push_back(north);
+		blocksEast.push_back(east);
+		pokemon.push_back(name);
 	}
 
 	return 0;
