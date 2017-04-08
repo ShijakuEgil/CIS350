6f9c0229556f1bfb6314699ca3f6e5b4dec09ce9
Egil Shijaku <eshijaku@umich.edu>
2017-04-07 21:43:37 -0400

    kenken first comit

--- KenKenPrg4CIS350/KenKenPrg4CIS350/Source.cpp
+++ KenKenPrg4CIS350/KenKenPrg4CIS350/Source.cpp
@@ -11,28 +11,55 @@ Description:
 #include<algorithm>
 using namespace std;
 enum OP_ALPHABET{ add = '+', sub = '-', mul = 'x', div = '/' };
+vector<pair<int, char>> cageOper;
+
 class KenKen
 {
+	int sizeOfBoard;
 	int board, cages;
 	char ** squares;
 public:
 	KenKen(int n, int c)
 	{
 		board = n;// board size
-		cages = c; //cages
-		squares = new char*[n];
+		cages = c;//cages
+		squares = new char * [n];
 		for (int i = 0; i < n; i++)// making 2 dim array to
 		{							// reapresent board
 			squares[i] = new char[n];
 		}
 	}
+
 	void add(char sqId, int i, int j)// adding identefiers to board square by square
 	{
 		squares[i][j] = sqId;
 	}
+
 	void addOperands(int opId, char operand)
 	{
-
+		pair<int, char> pair;
+		pair.first = opId;
+		pair.second = opId;
+		cageOper.push_back(pair);
+	}
+	void print()
+	{
+		cout << "operands :";
+		for (auto x : cageOper)
+		{
+			cout << x.first << " " << x.second << " ";
+		}
+		for (int i = 0; i < board; i++)
+		{
+			for (int j = 0; j < board; j++)
+			{
+				cout << squares[i][j];
+				if (j == board - 1)
+				{
+					cout << endl;
+				}
+			}
+		}
 	}
 };
 
@@ -53,6 +80,9 @@ int main()
 	for (int i = 0; i < cages; i++)
 	{
 		cin >> opId, operand;
+		newBoard.addOperands(opId, operand);
 	}
+
+	newBoard.print();
 	return 0;
 }
