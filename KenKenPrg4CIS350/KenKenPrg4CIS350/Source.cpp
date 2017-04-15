3a5b77adf7e9970f772a13941aeaecb2f0f98e99
Egil Shijaku <eshijaku@umich.edu>
2017-04-15 04:30:40 -0400

    tester commit

--- KenKenPrg4CIS350/KenKenPrg4CIS350/Source.cpp
+++ KenKenPrg4CIS350/KenKenPrg4CIS350/Source.cpp
@@ -8,72 +8,56 @@ Description:
 */
 
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<string>
+#include<vector>
 using namespace std;
 
-struct Cage
+class Board
 {
-	int result;
-	char operand, id;
-	vector<pair<int, int>> cellLoc;
-	Cage(char cellId, int row, int col)
-	{
-		id = cellId;
-		pair< int, int> p;
-		p.first = row;
-		p.second = col;
-		cellLoc.push_back(p);
-	}
-
-	void addCageResult(int cageResult, char operand)
-	{
-		result = cageResult;
-		this->operand = operand;
-	}
-	void addCageOp(int row, int col)
-	{
-		pair< int, int> p;
-		p.first = row;
-		p.second = col;
-		cellLoc.push_back(p);
-	}
-	bool checkCage(vector<vector<int>> board)// checks the cages if they are loaded 
-};
-class KenKen
-{
-	vector<vector<int>> kenKen;
-	vector<Cage> cages;
+	int size;
+	vector<vector<int>> board;
 	vector<int> numbers;
-	int size, nrCages;
 
-	bool findSolution(vector<vector<int>> board, vector<int> nrs, int row)//recursive function to get solution
+	bool findSolution(vector<vector<int>> ken, vector<int> nr, int row)
 	{
+		cout << "inside recursion" << endl;
+		bool check;
 		do
 		{
-			insertRow(board, nrs, row);//
-			if (checkRow(board, row))
+			insertRow(ken, nr, row);
+			if (checkRow(ken, nr, row) && row == size - 1)
 			{
-				vector<int> cagePos;
-				if (checkCage(board, row, cagePos ))
-				{
-
-				}
-				if (findSolution(board, nrs, row + 1))
-				{
-					return true;
-				}
+				return true;
 			}
-		} while (next_permutation(nrs.begin(), nrs.end())));
+			if (checkRow(ken, nr, row))
+			{
+				check = findSolution(ken, nr, row + 1);
+			}
+			if (check)
+			{
+				return true;
+			}
+		} while (next_permutation(nr.begin(), nr.end()));
 		return false;
 	}
-	bool checkRow(vector<vector<int>> board,  int row)
+
+	void insertRow(vector<vector<int>> & ken, vector<int> nr, int row)
 	{
-		for (int i = 0; i < row; i++)
+		cout << "inside insertRow" << endl;
+		for (int j = 0; j < size; j++)
 		{
-			for (int j = 0; j < size; j++)
+			ken[row][j] = nr[j];
+		}
+	}
+	bool checkRow(vector<vector<int>> ken, vector<int> nr, int row)
+	{
+		cout << " inside checkRow" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < row; j++)
 			{
-				if (board[i][j] == board[row][j])
+				if (ken[row][i] == ken[j][i])
 				{
 					return false;
 				}
@@ -81,97 +65,46 @@ class KenKen
 		}
 		return true;
 	}
-	bool checkCage(vector<vector<int>> board, int row, vector<int> & cagePos)
-	{
-		for (int i = 0; i < cages.size(); i++)
-		{
-			if (cages[i].isFull(board))
-			{
-				cagePos.push_back(i);
-				return true;
-			}
-		}
-	}
-	void InsertRow(vector<vector<int>> & board, vector<int> nrs, int row)
-	{
-		for (int i = 0; i < nrs.size(); i++)
-		{
-			board[row][i] == nrs[i];
-		}
-	}
-
 public:
-	KenKen(int sizeOfBoard, int nrOfCages)
+	Board(int size)
 	{
-		size = sizeOfBoard;
-		nrCages = nrOfCages;
-		for (int i = 1; i <= size; i++)
+		this->size = size;
+		for (int i = 0; i < size; i++)
 		{
-			numbers.push_back(i);
+			numbers[i] = i + 1;
+			cout << "constructor" << endl;
 		}
-	}
-	void addCellPosition(char cellid, int row, int col)
-	{
-		bool check = false;
-		if (cages.size() > 0)
-		{
-			for (int i = 0; i < cages.size(); i++)
-			{
-				if (cellid == cages[i].id)
-				{
-					check == true;
-					cages[i].addCageOp(row, col);
-				}
-			}
-			if (check == false)
-			{
-				Cage cage(cellid, row, col);
-				cages.push_back(cage);
-			}
-		}
-		else
-		{
-			Cage cage(cellid, row, col);
-			cages.push_back(cage);
-		}
-	}
-	void addCellOperand(int total, char operand, int i)
-	{
-		cages[i].addCageResult(total, operand);
 	}
 	void findSolution()
 	{
-		findsolution(kenKen, numbers, 0);
+		cout << " first findSolution" << endl;
+		if (findSolution(board, numbers, 0))
+		{
+			cout << " before print" << endl;
+			print();
+		}
+	}
+	void print()
+	{
+		cout << "inside print" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << board[i][j] << " ";
+			}
+			cout << endl;
+		}
 	}
 };
 
 int main()
 {
-	int sizeOfBoard, nrOfCages, cageOpResult;
-	char cellId, operand;
-	vector<vector<char>> board;
-	vector<Cage> cages;
-
-	cin >> sizeOfBoard >> nrOfCages;
-
-	if ((sizeOfBoard >= 0 && sizeOfBoard <= 9) &&
-		(nrOfCages >= sizeOfBoard && nrOfCages <= (pow(sizeOfBoard, 2) / 2)))
-	{
-		KenKen newBoard(sizeofBoard, nrOfcages);
-		for (int i = 0; i < sizeOfBoard; i++)
-		{
-			for (int j = 0; j < sizeOfBoard; j++)
-			{
-				cin >> cellId;
-				newBoard.addCellPosition(cellId, i, j);
-			}
-		}
-		for (int i = 0; i < nrOfCages; i++)
-		{
-			cin >> cageOpResult >> operand;
-			newBoard.addCellOperand(cageOpResult, operand, i);
-		}
-		newBoard.findSolution();
-	}
+	cout << "main" << endl;
+	int size;
+	cin >> size;
+	Board newBoard(size);
+	cout << " board created" << endl;
+	newBoard.findSolution();
 	return 0;
 }
--- kenkenTester/kenkenTester/Source.cpp
+++ kenkenTester/kenkenTester/Source.cpp
@@ -4,60 +4,245 @@
 #include<vector>
 using namespace std;
 
+struct Cage
+{
+	
+	char Id, oper;
+	int maxRow = -1;
+	int maxCol = -1;
+	int res;
+	vector<pair<int, int>> cageLoc;
+	Cage(char cellId, int row, int col)
+	{
+		maxRow = row;
+		maxCol = col;
+		Id = cellId;
+		pair<int, int> cellLoc;
+		cellLoc.first = row;
+		cellLoc.second = col;
+		cageLoc.push_back(cellLoc);
+	}
+	void addCellLoc(int row, int col)// adss locations of points that make up
+	{
+		if (row > maxRow)
+			maxRow = row;
+		if (col > maxCol)
+			maxCol = col;
+		pair<int, int> cellLoc;
+		cellLoc.first = row;
+		cellLoc.second = col;
+		cageLoc.push_back(cellLoc);
+	}
+	bool checkIfFull(int row)
+	{
+		if (row >= maxRow)
+			return true;
+		else
+			return false;
+	}
+	bool getResult(int ** board, int row)
+	{
+		if (checkIfFull(row))
+		{
+			vector<int> points;
+			int result = 0;
+			switch (oper)
+			{
+			case '+':
+				for (auto x : cageLoc)
+				{
+					result += board[x.first][x.second];
+				}
+				if (result == res)
+					return true;
+				else
+					return false;
+			case 'x':
+				result = 1;
+				for (auto x : cageLoc)
+				{
+					result *= board[x.first][x.second];
+				}
+				if (result == res)
+					return true;
+				else
+					return false;
+			case '/':
+				points.clear();
+				for (auto x : cageLoc)
+				{
+					points.push_back(board[x.first][x.second]);
+				}
+				sort(points.begin(), points.end());
+				result = points[1] / points[0];
+				if (result == res)
+					return true;
+				else
+					return false;
+			case '-':
+				points.clear();
+				for (auto x : cageLoc)
+				{
+					points.push_back(board[x.first][x.second]);
+				}
+				sort(points.begin(), points.end());
+				result = points[1] - points[0];
+				if (result == res)
+					return true;
+				else
+					return false;
+
+			}
+		}
+		else
+			return false;
+	}
+};
+
 class Board
 {
-	vector<vector<int>> board;
+	int boardSize, cages;
 	vector<int> numbers;
-	int boardSize;
-	
-	bool findSolution(vector<vector<int>> thisBoard, vector<int> thisNums, int row)
+	vector<Cage> cageObj;
+	int ** boards;
+
+	bool findSolution(int** board, vector<int> nr, int row)
 	{
-		do 
+		cout << "recursive " << row << endl;
+
+		bool check = false;
+		do
 		{
-			if (checkBoard(thisBoard, thisNums, row))
+			insertRow(nr, row);
+			if (checkRow(board, nr, row) && row == boardSize - 1 && )
 			{
-				for (int i = 0; i < boardSize; i++)
-				{
-					thisBoard[row][i] = thisNums[i];
-				}
-				return findSolution(thisBoard, thisNums, row++);
+				return true;
 			}
-			else 
-				return false;
-		} 
-while (next_permutation(thisBoard.begin(), thisBoard.end()));
+
+			if (checkRow(board, nr, row))
+			{
+				check = findSolution(board, nr, row + 1);
+			}
+			if (check)
+			{
+				return true;
+			}
+
+		} while (next_permutation(nr.begin(), nr.end()));
+
+		return false;
 	}
-	bool checkBoard(vector<vector<int>> thisBoard, vector<int> thisNums, int row)
+
+	void insertRow(vector<int> nr, int row)
+	{
+		for (int j = 0; j < boardSize; j++)
+		{
+			boards[row][j] = nr[j];
+		}
+		//print();
+	}
+
+	bool checkRow(int ** board, vector<int> nr, int row)
 	{
 		for (int i = 0; i < boardSize; i++)
 		{
-
+			for (int j = 0; j < row; j++)
+			{
+				if (boards[row][i] == boards[j][i])
+				{
+					return false;
+				}
+			}
 		}
+		return true;
 	}
-
 public:
-	Board(int size)
+	Board(int size, int nrCages)
 	{
+		cages = nrCages;
 		boardSize = size;
+		boards = new int * [size];
+		cageObj.resize(cages);
 		for (int i = 0; i < size; i++)
 		{
-			numbers[i] = i + 1;
+			boards[i] = new int[size];
+			numbers.push_back(i + 1);
 		}
+		
 	}
 
 	void findSolution()
 	{
-		bool solu;
-		solu = findSolution(board, numbers, 0);
+		if (findSolution(boards, numbers, 0))
+		{
+			print();
+		}
+		
 	}
-
+	void setCage(char cellId, int row, int col)
+	{
+		if (cageObj.size() == 0)
+		{
+			Cage newCage(cellId, row, col);
+			cageObj.push_back(newCage);
+		}
+		else
+		{
+			bool check = false;
+			for (auto x : cageObj)
+			{
+				if (cellId == x.Id)
+				{
+					x.addCellLoc(row, col);
+					check = true;
+				}
+			}
+			if (check == false)
+			{
+				Cage newCage(cellId, row, col);
+				cageObj.push_back(newCage);
+			}
+		}
+	}
+	void addCageOperands(int result, char oper, int i)
+	{
+		cageObj[i].res = result;
+		cageObj[i].oper = oper;
+	}
+	void print()
+	{
+		for (int i = 0; i < boardSize; i++)
+		{
+			for (int j = 0; j < boardSize; j++)
+			{
+				cout << boards[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
 };
 
 int main()
 {
-	int size;
-	cin >> size;
-	Board newBoard(size);
-	newBoard.findSolution();
+	int size, nrCages, operand;
+	char cellId;
+	cin >> size >> nrCages;
+	if ((size >= 3 && size <= 9) && (nrCages >= size && nrCages <= pow(size, 2) / 2))
+	{
+		Board newBoard(size, nrCages);
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cin >> cellId;
+			}
+		}
+		for (int i = 0; i <= nrCages; i++)
+		{
+			cin >> operand >> cellId;
+			newBoard.addCageOperands(operand, cellId, i);
+		}
+		newBoard.findSolution();
+	}
 	return 0;
 }
